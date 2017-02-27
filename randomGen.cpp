794c4758c2f06742dd51d6483e7cf11acc8a3777
Chaitanya <cayadav2009@gmail.com>
2017-02-27 09:16:53 +0530

    Update random generator

--- randomGen.cpp
+++ randomGen.cpp
@@ -5,48 +5,48 @@ using namespace std;
 
 int main()
 {
-  FILE* fp = fopen("data/inputGeekBoys.txt","w");
+  FILE* fp = fopen("random/inputGeekBoys.txt","w");
   int i=0;
   for(i=0;i<10;i++){
   	fprintf(fp,"G%d %d %d %d 0 %d\n",i,rand()%10,rand()%10,rand()%1000000,rand()%10);
   }
   fprintf(fp, "0 0 0 0 0 0\n");
-  fp = fopen("data/inputGenerousBoys.txt","w");
+  fp = fopen("random/inputGenerousBoys.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"Ge%d %d %d %d 0 %d\n",i,rand()%10,rand()%10,rand()%1000000,rand()%10);
   }
   fprintf(fp, "0 0 0 0 0 0\n");
-  fp = fopen("data/inputMiserBoys.txt","w");
+  fp = fopen("random/inputMiserBoys.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"M%d %d %d %d 0 %d\n",i,rand()%10,rand()%10,rand()%1000000,rand()%10);
   }
   fprintf(fp, "0 0 0 0 0 0\n");
-  fp = fopen("data/inputNormalGirls.txt","w");
+  fp = fopen("random/inputNormalGirls.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"N%d %d %d %d 0\n",i,rand()%10,rand()%10,rand()%1000000);
   }
   fprintf(fp, "0 0 0 0 0\n");
-  fp = fopen("data/inputDesperateGirls.txt","w");
+  fp = fopen("random/inputDesperateGirls.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"D%d %d %d %d 0\n",i,rand()%10,rand()%10,rand()%1000000);
   }
   fprintf(fp, "0 0 0 0 0\n");
-  fp = fopen("data/inputChoosyGirls.txt","w");
+  fp = fopen("random/inputChoosyGirls.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"C%d %d %d %d 0\n",i,rand()%10,rand()%10,rand()%1000000);
   }
   fprintf(fp, "0 0 0 0 0\n");
-  fp = fopen("data/inputEssentialGifts.txt","w");
+  fp = fopen("random/inputEssentialGifts.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"E%d %d %d \n",i,rand()%10000,rand()%10);
   }
   fprintf(fp, "0 0 0\n");
-  fp = fopen("data/inputUtilityGifts.txt","w");
+  fp = fopen("random/inputUtilityGifts.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"U%d %d %d %d\n",i,rand()%10000,rand()%10,rand()%100);
   }
   fprintf(fp, "0 0 0 0\n");
-  fp = fopen("data/inputLuxuryGifts.txt","w");
+  fp = fopen("random/inputLuxuryGifts.txt","w");
   for(i=0;i<10;i++){
   	fprintf(fp,"L%d %d %d %d %d\n",i,rand()%100000,rand()%10,rand()%10,rand()%10);
   }
